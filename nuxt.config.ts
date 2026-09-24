657f51263f2de0a5867620e6fd5956d29cf2e08d
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-09-24 11:02:43 +0300

    :sparkles: integrate css with scss architecture (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: [
-    '@vueuse/nuxt',
-  ],
-  css: [
-    '~/assets/styles/main.scss',
-  ],
+  modules: ['@vueuse/nuxt'],
+  css: ['~/assets/styles/tailwind.css', '~/assets/styles/main.scss'],
   vite: {
     css: {
       preprocessorOptions: {
@@ -17,7 +15,7 @@ export default defineNuxtConfig({
         },
       },
     },
+    plugins: [tailwindcss()],
   },
-
   devtools: { enabled: true },
 })
